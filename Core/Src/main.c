4800de20648180df07cec51e67d60783554f0b60
PIYUSH SURANI <surani.piyush316@gmail.com>
2026-02-07 01:52:05 +0000

    STM32F103C8T6 Bluepill Register Based Led Toggle, Simple Delay

---
+++ Core/Src/main.c
@@ -0,0 +1,51 @@
+#include "stm32f103xb.h" //use for register base programming
+
+void delay(uint32_t count); // simple delay function define
+
+int main(void)
+{
+   //Clock Setting
+    RCC->CR |= RCC_CR_HSEON; //bit 16 (High Speed External Clock ) ON of Clock Control Register
+    while (!(RCC->CR & RCC_CR_HSERDY)); // wait till external oscillator stable
+
+    //Flash Access
+    FLASH->ACR |= FLASH_ACR_LATENCY_2; // 2 wait cycle of CPU required for 72MHZ frequency to access flash
+    FLASH->ACR |= FLASH_ACR_PRFTBE; //to prefetch data from flash when cpu access other data
+
+    // PLL Setup
+    RCC->CFGR |= RCC_CFGR_PLLSRC;         // PLL source from external oscillator
+    RCC->CFGR |= RCC_CFGR_PLLMULL9;       // PLL Multiply with 9 to get 72MHZ Clock
+
+    // APB1 Setup
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV2; //set apb1 prescaler 2 to get max 36 MHZ
+
+    // Enable PLL
+    RCC->CR |= RCC_CR_PLLON; //start PLL
+    while (!(RCC->CR & RCC_CR_PLLRDY)); //wait till PLL Frequency stable
+
+
+    RCC->CFGR |= RCC_CFGR_SW_PLL; //PLL configure as system clock
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL); //wait till system clock = PLL
+
+
+
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;//GPIO PORTC clock enable
+
+
+    GPIOC->CRH &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13); // clear port 13 old setting
+    GPIOC->CRH |=  GPIO_CRH_MODE13_1;     // Port13 2Mz Push pull output set
+
+
+    while (1)
+    {
+        GPIOC->ODR ^= GPIO_ODR_ODR13;     // set and reset Bit 13 of ODR //togglr bit and so LED
+        delay(10000000); // Temporary delay
+    }
+}
+
+void delay(uint32_t count)
+{
+	    while (count--);
+	}
+
+
